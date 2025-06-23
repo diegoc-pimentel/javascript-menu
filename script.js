8832701c38006cb79eb7e2d5b37a2aefa7f25f9e
diegoc-pimentel <diego05.pimentel@gmail.com>
2025-06-23 05:42:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const container = document.querySelector('.menu-container');
+const addBtn = document.getElementById('add-item');
+const RemoveBtn = document.getElementById('remove-item');
+const filterBtn = document.getElementById('filter-items');
+const toggleThemeBtn = document.getElementById('toggle-theme');
+
+let menu = [
+  { name: "Array", category: "Main", image: "https://media.istockphoto.com/id/520410807/photo/cheeseburger.jpg?s=612x612&w=0&k=20&c=fG_OrCzR5HkJGI8RXBk76NwxxTasMb1qpTVlEM0oyg4=" },
+  { name: "Push", category: "Main", image: "https://png.pngtree.com/png-clipart/20240831/original/pngtree-vegetable-italian-slice-piece-pizza-vintage-png-image_15897644.png" },
+  { name: "Pop", category: "Dessert", image: "https://www.alecsicecream.com/cdn/shop/files/Alec_s_IceCream_OPEN_TVB.png?v=1713234762&width=1445" }
+];
+
+function renderMenu(items){
+    container.innerHTML = items.map(item => `
+        <div class="menu-item">
+            <img src="${item.image}" alt="${item.name}">
+            <h3>${item.name}</h3>
+            <p>${item.category}</p>
+        </div>
+        `).join('');
+}
+renderMenu(menu);
+
+
+// PUSH & POP
+addBtn.addEventListener('click', () => {
+    const newDish = {
+        name: "New Dish",
+        category: "Main",
+        image: "https://picsum.photos/200/150?random=" + Math.floor(Math.random() * 1000)
+    };
+
+    menu.push(newDish);
+    renderMenu(menu);
+
+    RemoveBtn.addEventListener('click', () => {
+    menu.pop();
+    renderMenu(menu);
+    })
+});
+
+//FILTER
+
+filterBtn.addEventListener('click', () => {
+    const desserts = menu.filter(item => item.category === "Dessert");
+    renderMenu(desserts);
+});
+
+toggleThemeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+if(document.body.classList.contains('dark-mode')){
+    localStorage.setItem('theme', 'dark');
+} else {
+    localStorage.setItem('theme', 'light');
+}
